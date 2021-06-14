9b75a86ba36ad45cce4b926420010b013353f1f7
nhat51 <65001571+nhat51@users.noreply.github.com>
2021-06-14 20:44:31 +0700

    update1

---
+++ Pratical/Pratical/Pratical/Pages/EmployeeList.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Pratical.models;
+
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Pratical.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class EmployeeList : Page
+    {
+        public EmployeeList()
+        {
+            this.InitializeComponent();
+            
+        }
+
+        private void List_Employee_Loaded(object sender, RoutedEventArgs e)
+        {
+            string FilePath = Path.Combine(Package.Current.InstalledLocation.Path, "employee.json");
+            using (StreamReader file = File.OpenText(FilePath))
+            {
+                var json = file.ReadToEnd();
+                Dictionary<string, object> result = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
+                string contacts = result["employee_list"].ToString();
+                List<EmployeeModel> objResponse = Newtonsoft.Json.JsonConvert.DeserializeObject<List<EmployeeModel>>(contacts);
+                EmployeeItems.ItemsSource = objResponse;
+            }
+        }
+
+    }
+    
+}
---
+++ Pratical/Pratical/Pratical/models/EmployeeModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Pratical.models
+{
+    class EmployeeModel
+    {
+        public string name { get; set; }
+        public string role { get; set; }
+        public int birthyear { get; set; }
+    }
+}
